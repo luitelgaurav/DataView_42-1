700247002b2067ec6ff408791a17b91af6770127
Dioguini97 <91206234+Dioguini97@users.noreply.github.com>
2022-04-17 03:13:53 +0100

    Update Project_App.py

--- Project_App.py
+++ Project_App.py
@@ -13,6 +13,8 @@ import urllib.request
 from PIL import Image
 import requests
 from io import BytesIO
+import pycountry
+from datetime import date
 
 # https://htmlcheatsheet.com/css/
 
@@ -21,6 +23,7 @@ from io import BytesIO
 path_data = 'https://raw.githubusercontent.com/Dioguini97/DataView_42/main/Data/'
 
 df_players = pd.read_csv(path_data + 'NBA_Players_With_Team.csv')
+df_players_info = pd.read_csv(path_data + 'NBA_Players_Info.csv')
 df_teams = pd.read_csv(path_data + 'NBA_Teams.csv')
 shooting_stats = pd.read_csv(path_data + 'NBA_Active_Players_Data_14-04-2022.csv')
 players_stats = pd.read_csv(path_data + 'NBA_Players_Stats_Data_14-04-2022.csv')
@@ -36,7 +39,7 @@ img_court = Image.open("nba_court.jpg")
 nba_logo = path_img + 'NBA_LOGO.png'
 
 shooting_categories = ['FGM', 'FGA', 'FG3M', 'FG3A',  'FTM', 'FTA']
-shooting_categories_percent = ['FG_PCT' , 'FG3_PCT' , 'FT_PCT']
+shooting_categories_percent = ['FG_PCT', 'FG3_PCT', 'FT_PCT']
 
 
 ######################################################Interactive Components############################################
@@ -52,7 +55,7 @@ dropdown_players = dcc.Dropdown(
 
 dropdown_quarters = dcc.Dropdown(
         id='quarters_drop',
-        options=['All Quarters','1st Quarter','2nd Quarter','3rd Quarter','4th Quarter','5th Quarter','6th Quarter','7th Quarter'],
+        options=['All Quarters', '1st Quarter', '2nd Quarter', '3rd Quarter', '4th Quarter', '5th Quarter', '6th Quarter', '7th Quarter'],
         value='All Quarters'
     )
 
@@ -65,84 +68,121 @@ server = app.server
 
 app.layout = html.Div([
 
+    ###HEADER###
     html.Div([
-            html.H1('NBA Current Season Dashboard'), html.Img(src=nba_logo, id='nba_logo'), html.Hr()]
-       , id='1st_row'),
+        html.H1('NBA Current Season Dashboard'),
+        html.Img(src=nba_logo, id='nba_logo')
+    ], id='st_row'),
 
+    html.Hr(),
+    ###FILTER###
     html.Div([
-        html.Div([
-            html.Div(
-                html.Label('Player Choice')
-            ),
-            html.Div(
-                dropdown_players
-            )
-        ], className='DropdownDiv'),
 
         html.Div([
-            html.Div(
-                html.Label('Quarter Choice')
-            ),
-            html.Div(
-                dropdown_quarters
-            )
-        ], className='DropdownDiv'),
-        html.Hr()], id='filterDiv'
-    ),
-    
+            html.Label('Player Choice'),
+            dropdown_players
+        ], className='DropDownDiv'),
+
+        html.Div([
+            html.Label('Quarter Choice'),
+            dropdown_quarters
+        ], className='DropDownDiv')
+    ], id='filterDiv'),
+
+    html.Hr(),
+    ###STATS###
     html.Div([
 
         html.Div([
             html.Img(id='player_img'),
-            html.Div(
-               [html.P(html.Label('Player Name', id='Player_Name')), html.P(html.Label('Player Height', id='Player_Height')),
-                html.P(html.Label('Player Team', id='Player_Team')), html.P(html.Label('MVP Prize', id='MVP_Prize'))], id='player_text'
-            )], id='player_info'),
+            html.Img(id='flag_country'),
+
+            html.Div([
+                html.P([
+                    html.Label('Name: ', className='player_text_lab'),
+                    html.Label('Player Name', id='Player_Name')
+                ]),
+                html.P([
+                    html.Label('Height: ', className='player_text_lab'),
+                    html.Label('Player Height', id='Player_Height')
+                ]),
+                html.P([
+                    html.Label('Team: ', className='player_text_lab'),
+                    html.Label('Player Team', id='Player_Team')
+                ]),
+                html.P([
+                    html.Label('# MVP: ', className='player_text_lab'),
+                    html.Label('MVP Prize', id='MVP_Prize')
+                ]),
+                html.P([
+                    html.Label('Age: ', className='player_text_lab'),
+                    html.Label('Player Age', id='Player_Age')
+                ]),
+                html.P([
+                    html.Label('Position: ', className='player_text_lab'),
+                    html.Label('Player Position', id='Player_Position')
+                ]),
+                html.P([
+                    html.Label('Weight: ', className='player_text_lab'),
+                    html.Label('Player Weight', id='Player_Weight')
+                ]),
+                html.P([
+                    html.Label('Active Since: ', className='player_text_lab'),
+                    html.Label('Player Active Since', id='Player_Active_Since')
+                ]),
+                html.P([
+                    html.Label(id='Player_Jersey')
+                ])
+            ], id='player_text')
+
+        ], id='player_info'),
 
         html.Div([
-            html.Label('Player Sucess Shooting Performance'),
-            dcc.Graph(
-                    id='player_sucess_shooting_graph'
-                    )
-            ],style={'width': '49%', 'display': 'inline-block'}),
+
+            html.Div([
+                html.Label('Player Sucess Shooting Performance'),
+                dcc.Graph(id='player_sucess_shooting_graph')], className='graph_shoot'),
+            html.Div([
+                html.Label('Player Fail Shooting Performance'),
+                dcc.Graph(id='player_fail_shooting_graph')], className='graph_shoot')
+        ], id='DivShootGraph'),
+
         html.Div([
-            html.Label('Player Fail Shooting Performance'),
-            dcc.Graph(
-                    id='player_fail_shooting_graph'
-                    )
-            ],style={'width': '49%', 'display': 'inline-block'}),
 
-        
-    ], id='showDiv'),
-    html.Div([
-            html.Label('Player Shooting Stats'),
-            dcc.Graph(
-                    id='player_shootings_stats'
-                    ),
-            dcc.Graph(
-                    id='player_shootings_stats(%)'
-                    ),
-            dcc.Graph(
-                    id='player_others_stats'
-                    )
-            ]),
-            
-])
+            html.Div([
+                html.Label('Player vs Team'),
+                dcc.Graph(id='player_shootings_stats')], className='graph_radar'),
+            html.Div([
+                html.Label('Player vs Team'),
+                dcc.Graph(id='player_shootings_stats_per')], className='graph_radar')
+            ], id='DivRadarPlot'),
 
+        html.Div(dcc.Graph(id='player_others_stats'), className='graph')
+
+    ], id='showDiv')
+], id='main')
 
 ######################################################Callbacks#########################################################
 
 @app.callback(
     [
     Output(component_id='player_img', component_property='src'),
+    Output(component_id='flag_country', component_property='src'),
+
     Output(component_id='Player_Name', component_property='children'),
     Output(component_id='Player_Height', component_property='children'),
     Output(component_id='Player_Team', component_property='children'),
     Output(component_id='MVP_Prize', component_property='children'),
+    Output(component_id='Player_Age', component_property='children'),
+    Output(component_id='Player_Position', component_property='children'),
+    Output(component_id='Player_Weight', component_property='children'),
+    Output(component_id='Player_Active_Since', component_property='children'),
+    Output(component_id='Player_Jersey', component_property='children'),
+
     Output(component_id='player_sucess_shooting_graph', component_property='figure'),
     Output(component_id='player_fail_shooting_graph', component_property='figure'),
     Output(component_id='player_shootings_stats', component_property='figure'),
-    Output(component_id='player_shootings_stats(%)', component_property='figure'),
+    Output(component_id='player_shootings_stats_per', component_property='figure'),
     Output(component_id='player_others_stats', component_property='figure'),
     ],
     [Input(component_id='players_drop', component_property='value'),
@@ -156,8 +196,29 @@ def shooting_plots(selected_player,quarter):
     player_img_url='https://cdn.nba.com/headshots/nba/latest/1040x760/'+str(player_id)+'.png'
 
     player_name = df_players[df_players['id'] == player_id]['full_name'].values[0]
+    team_id = df_players[df_players['id'] == player_id]['team_id'].values[0]
+    player_team = df_teams[df_teams['id'] == team_id]['full_name'].values[0]
 
+    player_height = df_players_info[df_players_info['PERSON_ID'] == player_id]['HEIGHT'].values[0]
+    player_height = str(round((float(player_height[0])*30.48 + float(player_height[-1])*2.54)/100, 2)) + ' m'
 
+    player_country = df_players_info[df_players_info['PERSON_ID'] == player_id]['COUNTRY'].values[0]
+
+    if player_country == 'USA':
+        player_country = 'United States'
+
+    country = pycountry.countries.get(name=player_country)
+
+    player_flag_url = 'https://raw.githubusercontent.com/hampusborgos/country-flags/main/png250px/' + country.alpha_2.lower() + '.png'
+
+    player_birthday = df_players_info[df_players_info['PERSON_ID'] == player_id]['BIRTHDATE'].values[0]
+    player_age = date.today().year - int(player_birthday[:4]) - ((date.today().month, date.today().day) < (int(player_birthday[5:7]), int(player_birthday[-2:])))
+
+    player_position = df_players_info[df_players_info['PERSON_ID'] == player_id]['POSITION'].values[0]
+    player_weigth = df_players_info[df_players_info['PERSON_ID'] == player_id]['WEIGHT'].values[0]
+    player_weigth = str(round(player_weigth * 0.45359237, 2)) + 'kg'
+    player_since = df_players_info[df_players_info['PERSON_ID'] == player_id]['FROM_YEAR'].values[0]
+    player_jersey = df_players_info[df_players_info['PERSON_ID'] == player_id]['JERSEY'].values[0]
 
     #Create sucess shooting graph
     img_width = 500
@@ -255,8 +316,8 @@ def shooting_plots(selected_player,quarter):
     radar_plot_stats.update_layout(
         polar=dict(
             radialaxis=dict(
-            visible=True,
-            range=[0, max(players_avg_stats['FGA'].max(),players_avg_stats['FTA'].max(),players_avg_stats['FG3A'].max())]
+                visible=True,
+                range=[0, max(players_avg_stats['FGA'].max(),players_avg_stats['FTA'].max(),players_avg_stats['FG3A'].max())]
             )),
         showlegend=False
     )
@@ -318,9 +379,8 @@ def shooting_plots(selected_player,quarter):
                  color="Who", barmode="group" ,orientation='h' )
 
 
-    print(player_id)
 
-    return player_img_url,player_name,player_name,player_name,player_name,fig_sucess,fig_fail,radar_plot_stats,radar_plot_stats_perc,bar_plot_other_stats
+    return player_img_url,player_flag_url,player_name,player_height,player_team,player_name,player_age,player_position,player_weigth,player_since,player_jersey,fig_sucess,fig_fail,radar_plot_stats,radar_plot_stats_perc,bar_plot_other_stats
 
    
 
